477f862b5e315b8467d3955d88bbbd4250f9b0fc
Andre Alexandre <andre.alexandre@workday.com>
2022-01-21 21:20:36 +0000

    refactor zookeeper client factory

--- src/test/scala/example/ZooKeeperClientSpec.scala
+++ src/test/scala/example/ZooKeeperClientSpec.scala
@@ -10,17 +10,23 @@ import java.util.concurrent.CountDownLatch
 
 class ZooKeeperClientSpec extends ZooKeeperBaseSpec with should.Matchers with StrictLogging {
 
-  "Zookeeper client" should "connect to container" in {
-    val sessionTimeout = 2000
+  def zooKeeperClientFactory(sessionTimeout: Int = 2000): ZooKeeper = {
     val countDownLatch = new CountDownLatch(1)
-
-    val zookeeper = new ZooKeeper(zookeeperContainer.getConnectString(), sessionTimeout, (event: WatchedEvent) => {
+    val zooKeeper = new ZooKeeper(zookeeperContainer.getConnectString(), sessionTimeout, (event: WatchedEvent) => {
       logger.info(s"Zookeeper event=$event")
       if (event.getState == KeeperState.SyncConnected) {
         countDownLatch.countDown()
       }
     })
     countDownLatch.await()
+    zooKeeper
+  }
+
+  "Zookeeper client" should "connect to container" in {
+    val sessionTimeout = 2000
+    val countDownLatch = new CountDownLatch(1)
+
+    val zookeeper = zooKeeperClientFactory()
 
     zookeeperContainer.getContainerId should not be(empty)
     zookeeper.getState should be(ZooKeeper.States.CONNECTED)
@@ -29,25 +35,13 @@ class ZooKeeperClientSpec extends ZooKeeperBaseSpec with should.Matchers with St
   it should "create a new ephemeral znode" in {
     val sessionTimeout = 2000
 
-    val clientFactory = () => {
-      val countDownLatch = new CountDownLatch(1)
-      val zooKeeper = new ZooKeeper(zookeeperContainer.getConnectString(), sessionTimeout, (event: WatchedEvent) => {
-        logger.info(s"Zookeeper event=$event")
-        if (event.getState == KeeperState.SyncConnected) {
-          countDownLatch.countDown()
-        }
-      })
-      countDownLatch.await()
-      zooKeeper
-    }
-
-    val client1 = clientFactory()
+    val client1 = zooKeeperClientFactory()
     client1.create("/test", "test data".toArray.map(_.toByte), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL)
     val data = client1.getData("/test", false, null)
     new String(data, Charset.defaultCharset()) should be("test data")
     client1.close()
 
-    val client2 = clientFactory()
+    val client2 = zooKeeperClientFactory()
     val stat = client2.exists("/test", false)
     stat should be(null)
   }
@@ -55,25 +49,13 @@ class ZooKeeperClientSpec extends ZooKeeperBaseSpec with should.Matchers with St
   it should "create a new persistent znode" in {
     val sessionTimeout = 2000
 
-    val clientFactory = () => {
-      val countDownLatch = new CountDownLatch(1)
-      val zooKeeper = new ZooKeeper(zookeeperContainer.getConnectString(), sessionTimeout, (event: WatchedEvent) => {
-        logger.info(s"Zookeeper event=$event")
-        if (event.getState == KeeperState.SyncConnected) {
-          countDownLatch.countDown()
-        }
-      })
-      countDownLatch.await()
-      zooKeeper
-    }
-
-    val client1 = clientFactory()
+    val client1 = zooKeeperClientFactory()
     client1.create("/test", "test data".toArray.map(_.toByte), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
     val data = client1.getData("/test", false, null)
     new String(data, Charset.defaultCharset()) should be("test data")
     client1.close()
 
-    val client2 = clientFactory()
+    val client2 = zooKeeperClientFactory()
     val readData = client2.getData("/test", false, null)
     new String(readData, Charset.defaultCharset()) should be ("test data")
   }
